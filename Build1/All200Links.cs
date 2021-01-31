ae4cf6c454dc9ed400f2d15d72239a29c507af8c
natalia_hrytsak <nataliah@wizardsdev.com>
2021-01-31 17:06:52 +0200

    31.01

--- Build1/All200Links.cs
+++ Build1/All200Links.cs
@@ -8,13 +8,13 @@ namespace SEOchecklistKyiv.Methods
 {
     public class All200Links:Base
     {      
-    
+        [Test]
         public void GetAll200()
         {          
             All_Pages.GetAllLinks();
             for (int i = 0; i < All_Pages.siteAllLinks.Count; i++)
             {
-                Page_StatusCode.GetResponse(All_Pages.siteAllLinks[i].ToString());               
+                Page_StatusCode.GetResponse(All_Pages.siteAllLinks[i].ToString());                
             }
         }
     }
